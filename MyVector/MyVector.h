4121cb22d561c4615be4bc4a364fe508072622f6
zzttyy413841467 <zzttyy413841467@gmail.com>
2020-10-30 17:59:19 +0800

    新建dot

--- MyVector/MyVector.h
+++ MyVector/MyVector.h
@@ -1,1 +1,26 @@
-#pragma once
+
+#ifndef MYVECTOR_H
+#define MYVECTOR_H
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class MyVector
+{
+public:
+	MyVector();
+	~MyVector();
+	friend double dot(vector<double> a, vector<double> b);
+private:
+
+};
+
+
+
+double dot(vector<double> a, vector<double> b);
+
+
+#endif // !MYVECTOR_H
+
+
---
+++ MyVector/main.cpp
@@ -0,0 +1,15 @@
+#include "MyVector.h"
+
+
+int main()
+{
+	
+	vector<double> a = { 1,2,3 };
+	vector<double> b = { 1,2,3 };
+
+	cout << dot(a, b);
+
+	cout << "Hello World!\n";
+
+	return 0;
+}
